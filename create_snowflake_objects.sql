fd0a0ab8cbaef31073fbf050ae62f47584b6bc72
vrangayyan6 <vrangayyan@hotmail.com>
2024-11-14 21:52:45 -0500

    Add files via upload

---
+++ create_snowflake_objects.sql
@@ -0,0 +1,72 @@
+/*--
+• Database, schema, warehouse, and stage creation
+--*/
+
+USE ROLE SECURITYADMIN;
+
+CREATE ROLE cortex_user_role;
+-- GRANT DATABASE ROLE SNOWFLAKE.CORTEX_USER TO ROLE cortex_user_role;
+
+-- GRANT ROLE cortex_user_role TO USER <user>;
+
+USE ROLE sysadmin;
+
+-- Create demo database
+CREATE OR REPLACE DATABASE cortex_analyst_demo;
+
+-- Create schema
+CREATE OR REPLACE SCHEMA cortex_analyst_demo.revenue_timeseries;
+
+-- Create warehouse
+CREATE OR REPLACE WAREHOUSE cortex_analyst_wh
+    WAREHOUSE_SIZE = 'large'
+    WAREHOUSE_TYPE = 'standard'
+    AUTO_SUSPEND = 60
+    AUTO_RESUME = TRUE
+    INITIALLY_SUSPENDED = TRUE
+COMMENT = 'Warehouse for Cortex Analyst demo';
+
+-- GRANT USAGE ON WAREHOUSE cortex_analyst_wh TO ROLE cortex_user_role;
+-- GRANT OPERATE ON WAREHOUSE cortex_analyst_wh TO ROLE cortex_user_role;
+
+-- GRANT OWNERSHIP ON SCHEMA cortex_analyst_demo.revenue_timeseries TO ROLE cortex_user_role;
+-- GRANT OWNERSHIP ON DATABASE cortex_analyst_demo TO ROLE cortex_user_role;
+
+
+USE ROLE cortex_user_role;
+
+-- Use the created warehouse
+USE WAREHOUSE cortex_analyst_wh;
+
+USE DATABASE cortex_analyst_demo;
+USE SCHEMA cortex_analyst_demo.revenue_timeseries;
+
+-- Create stage for raw data
+CREATE OR REPLACE STAGE raw_data DIRECTORY = (ENABLE = TRUE);
+
+/*--
+• Fact and Dimension Table Creation
+--*/
+
+-- Fact table: daily_revenue
+CREATE OR REPLACE TABLE cortex_analyst_demo.revenue_timeseries.daily_revenue (
+    date DATE,
+    revenue FLOAT,
+    cogs FLOAT,
+    forecasted_revenue FLOAT,
+    product_id INT,
+    region_id INT
+);
+
+-- Dimension table: product_dim
+CREATE OR REPLACE TABLE cortex_analyst_demo.revenue_timeseries.product_dim (
+    product_id INT,
+    product_line VARCHAR(16777216)
+);
+
+-- Dimension table: region_dim
+CREATE OR REPLACE TABLE cortex_analyst_demo.revenue_timeseries.region_dim (
+    region_id INT,
+    sales_region VARCHAR(16777216),
+    state VARCHAR(16777216)
+);
